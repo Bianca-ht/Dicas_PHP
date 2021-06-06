6f95e786ce2149d13a058872e39e2afce9da50e4
Bianca Alves <78497908+Bianca-ht@users.noreply.github.com>
2021-06-06 14:27:36 -0300

    Add files via upload

---
+++ 01-Comando_Bdados_PHP/updateUsuarios.php
@@ -0,0 +1,30 @@
+﻿
+
+<?php
+
+	$id=10;
+	$nome= "Ricardo";
+
+    $dsn='mysql:host=localhost;dbname=php_com_pdo';
+    $user='root';
+    $password='';
+    
+    try{
+        $conexao = new PDO($dsn, $user, $password );
+        $query=$conexao->prepare('update tb_usuarios set nome = :nome where id= :id'); 
+        $query->execute(array( 
+			':id' => $id,
+			':nome' =>$nome
+		));
+    
+        
+    }catch (PDOException $e){
+        echo 'Erro: '.$e->getCode().'Mensagem: '.$e->getMessage();
+    }
+    
+?>
+
+
+
+
+
