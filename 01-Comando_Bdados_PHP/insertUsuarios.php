05a7db3828a9ad6fd6e0e81d0dad9e3e9dd41cc7
Bianca Alves <78497908+Bianca-ht@users.noreply.github.com>
2021-06-06 14:21:05 -0300

    Add files via upload

---
+++ 01-Comando_Bdados_PHP/insertUsuarios.php
@@ -0,0 +1,26 @@
+﻿
+
+<?php
+
+    $dsn='mysql:host=localhost;dbname=php_com_pdo';
+    $user='root';
+    $password='';
+    
+    try{
+        $conexao = new PDO($dsn, $user, $password );
+        $query=$conexao->prepare('insert into tb_usuarios( nome, email, senha) values (:nome, :email, :senha)');
+		$query->execute(array(
+			':nome'=> 'Isa',
+			':email'=> 'Isa@hotmail.com',
+			':senha'=> '123456',
+			));
+        
+    }catch (PDOException $e){
+        echo 'Erro: '.$e->getCode().'Mensagem: '.$e->getMessage();
+    }
+  
+?>
+
+
+
+
---
+++ 01-Comando_Bdados_PHP/insertVarios.php
@@ -0,0 +1,34 @@
+﻿
+
+<?php
+
+    $dsn='mysql:host=localhost;dbname=php_com_pdo';
+    $user='root';
+    $password='';
+    
+    try{
+        
+		$conexao = new PDO($dsn, $user, $password );
+        $query=$conexao->prepare('insert into tb_usuarios( nome, email, senha) values (:nome, :email, :senha)');
+		$query->execute(array(
+			':nome'=> 'Carlos Alberto',
+			':email'=> 'c_alberto@hotmail.com',
+			':senha'=> '123456',
+			));
+		
+		$conexao = new PDO($dsn, $user, $password );
+        $query=$conexao->prepare('insert into tb_usuarios( nome, email, senha) values (:nome, :email, :senha)');
+		$query->execute(array(
+			':nome'=> 'Sarah Castro',
+			':email'=> 'castro_sarah@hotmail.com',
+			':senha'=> '123456',
+			));
+		      
+    }catch (PDOException $e){
+        echo 'Erro: '.$e->getCode().'Mensagem: '.$e->getMessage();
+    }
+?>	
+
+
+
+
