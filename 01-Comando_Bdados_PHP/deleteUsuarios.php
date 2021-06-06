47ecea524096f36c36774dd0bd133b0f87ccf6a0
Bianca Alves <78497908+Bianca-ht@users.noreply.github.com>
2021-06-06 20:05:33 -0300

    Add files via upload

---
+++ 01-Comando_Bdados_PHP/deleteUsuarios.php
@@ -0,0 +1,26 @@
+﻿
+
+<?php
+	
+	$id=6;
+
+    $dsn='mysql:host=localhost;dbname=php_com_pdo';
+    $user='root';
+    $password='';
+    
+    try{
+        $conexao = new PDO($dsn, $user, $password );
+        $query=$conexao->prepare('delete from tb_usuarios where id= :id');
+        $query->bindParam(':id', $id);
+		$query->execute();
+		
+    }catch (PDOException $e){
+        echo 'Erro: '.$e->getCode().'Mensagem: '.$e->getMessage();
+    }
+   
+?>
+
+
+
+
+
